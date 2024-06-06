4a5194180f9407442a5e77a05fd71c265d89d28d
josephine352 <129162101+josephine352@users.noreply.github.com>
2024-06-06 11:35:24 +0200

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,16 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-	
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
+    basic.setLedColor(0x00ff00)
 })
-basic.forever(function () {
-	
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    null.showImage(0, 400)
+    basic.setLedColor(0x007fff)
+})
+input.onButtonEvent(Button.AB, input.buttonEventClick(), function () {
+    basic.setLedColor(0xff0000)
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.setLedColor(0xffff00)
+})
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
+    basic.setLedColor(0xff0080)
 })
